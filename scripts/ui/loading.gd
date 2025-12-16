470a84ed6dc85593f6b74241ce4539df204c7d5d
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-12-16 22:18:41 +0100

    Add fade animation to loading screen

--- scripts/ui/loading.gd
+++ scripts/ui/loading.gd
@@ -1,7 +1,13 @@
 extends Control
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $Components/Gift/AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event.is_action("lmb"):
 		animated_sprite_2d.play("open")
+		await get_tree().create_timer(0.75).timeout
+		animation_player.play("switch_scene")
+
+func load_main_menu():
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
